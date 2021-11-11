d114a4beb558dafb99a832902c28317cad52d2d5
Zet-work <90544011+Zet-work@users.noreply.github.com>
2021-11-11 22:06:48 +0530

    Add files via upload

---
+++ Addition of 2 number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+int a,b,c;
+clrscr();
+printf ("enter the number\n");
+scanf ("%d",&a);
+printf ("enter the number\n");
+
+scanf ("%d",&b);
+c=a+b;
+printf ("%d+%d=%d",a,b,c);
+return 0;
+}
