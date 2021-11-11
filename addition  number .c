f2bf6bd49a6322fd1a315483716c47f7fdfae4ee
Zet-work <90544011+Zet-work@users.noreply.github.com>
2021-11-11 22:05:54 +0530

    Add files via upload

---
+++ addition  number .c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+int main ()
+{
+int num1,num2,sum;
+clrscr();
+  printf ("enter 2 any number");
+  scanf ("%d %d",&num1,&num2);
+ sum=num1+num2;
+  printf ("%d+%d=%d",num1,num2,sum);
+  return 0;
+}
